dd9ea9e90c0f16c0fc287be7e74be7076fc5815f
Chris <chrislee0419@gmail.com>
2021-06-15 07:36:26 +0100

    version bump and manifest updates

--- hellosSearch/Properties/AssemblyInfo.cs
+++ hellosSearch/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hellosSearch")]
+[assembly: AssemblyTitle("hello's Search")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hellosSearch")]
+[assembly: AssemblyProduct("hello's Search")]
 [assembly: AssemblyCopyright("Copyright © hello 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
